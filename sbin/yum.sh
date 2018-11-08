168b5246b9455efa877867f076b08a150224e4a5
root <root@room9pc01.tedu.cn>
2018-11-08 12:44:10 +0800

    check

--- sbin/yum.sh
+++ sbin/yum.sh
@@ -10,13 +10,16 @@ enabled=1
 gpgcheck=0" > /etc/yum.repos.d/development.repo
 }
 Check_Yum(){
-	Yumcheck=0
-	echo $Yumcheck
-	while [ $Yumcheck -eq 0 ]
+	YumCheck=`yum repolist | sed -n '/repolist/p'|awk -F: '{print $2}'|sed 's/,//'`
+	while [ $YumCheck -eq 0 ]
 	do
+		Yum
 		yum clean all &> /dev/null
 		YumCheck=`yum repolist | sed -n '/repolist/p'|awk -F: '{print $2}'|sed 's/,//'`
-		Yum
+		if [ $YumCheck -ne 0 ];then
+			echo -e "\033[31mYum源检测正常\033[0m"
+			exit 0
+		fi
 	done
 }
 #检测是否配置yum
@@ -25,10 +28,5 @@ YumCheck1=`yum repolist &> /dev/null`
 if [ $? -ne 0 ];then
    Yum
 fi
-YumCheck2=`yum repolist | sed -n '/repolist/p'|awk -F: '{print $2}'|sed 's/,//'`
-if [ $YumCheck2 -eq 0 ];then
-	Check_Yum
-else
-    echo -e "\033[31mYum源检测正常\033[0m"
-fi
+Check_Yum 
 
